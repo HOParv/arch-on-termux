5b068cec818a762f8f06233d743bb7edd8271681
Xero13 <javierpescadornunez@gmail.com>
2025-10-03 18:16:21 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,31 @@
+apt update
+
+apt upgrade -y
+
+apt install proot-distro
+
+echo '
+
+
+
+PROOT-DISTRO INSTALLED
+
+
+
+'
+sleep 7
+
+proot-distro install archlinux 
+
+echo '
+
+
+
+
+ARCH LINUX INSTALLED
+
+login with 'proot-distro login archlinux'
+
+
+'
+
